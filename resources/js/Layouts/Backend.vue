c0372b1499dac3df64526abbc9b47a6fda73f34a
MdIshtiaque <ishtiaqueferdous109@gmail.com>
2024-08-28 08:53:34 +0600

    Icon for sidebar hide and show

--- resources/js/Layouts/Backend.vue
+++ resources/js/Layouts/Backend.vue
@@ -1,6 +1,7 @@
 <script setup>
 import { ref } from 'vue';
-import { XMarkIcon } from '@heroicons/vue/24/solid';
+import { ChevronRightIcon } from '@heroicons/vue/24/solid';
+import { ChevronLeftIcon } from "@heroicons/vue/24/solid";
 
 const isSidebarVisible = ref(true);
 
@@ -12,19 +13,17 @@ const toggleSidebar = () => {
 <template>
     <div class="flex w-full h-screen">
         <!-- Sidebar -->
-        <transition name="slide">
-            <div v-if="isSidebarVisible" class="w-[15%] h-full bg-blue-950">
+            <div :class="isSidebarVisible ? 'w-[15%]' : 'w-0'" class="h-full bg-blue-950 transition-width duration-1000 ease overflow-hidden">
                 <!-- Sidebar content here -->
             </div>
-        </transition>
 
         <!-- Main content area -->
-        <div :class="isSidebarVisible ? 'w-[85%]' : 'w-full'" class="h-full transition-all duration-300 ease-in-out">
+        <div :class="isSidebarVisible ? 'w-[85%]' : 'w-full'" class="h-full transition-width duration-1000 ease">
             <div class="w-full h-16 bg-indigo-600 flex justify-between items-center p-5">
                 <!-- Toggle Button -->
                 <button @click="toggleSidebar" class="text-white focus:outline-none">
-                    <MenuIcon v-if="!isSidebarVisible" class="h-6 w-6" />
-                    <XMarkIcon v-else class="h-6 w-6" />
+                    <ChevronRightIcon v-if="!isSidebarVisible" class="h-6 w-6" />
+                    <ChevronLeftIcon v-else class="h-6 w-6" />
                 </button>
                 <!-- Other top bar content -->
             </div>
